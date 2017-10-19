67d08a22314b6d5fcfddc729f7187bba33809292
Pietro Degrazia <pietrodegrazia@hotmail.com>
2017-10-19 10:10:43 -0200

    adicionando taxa de bit por segundo

--- tcp/client.c
+++ tcp/client.c
@@ -11,12 +11,16 @@
 #define INVALID_SOCKET ((SOCKET)~0)
 
 #define CLIENT_PORT 2345 // porta TCP do cliente
-
+#define BUFFER_SIZE_BYTES 1250
+#define BUFFER_SIZE_BITS 10000
 int main(int argc, char* argv[]) {
     SOCKET socket_fd;
     struct sockaddr_in client, server;
     int i, server_port;
-    char server_ip[16], buffer[1250];
+    char server_ip[16], buffer[BUFFER_SIZE_BYTES];
+
+    int count;
+    time_t current_time, start_time;
     
     if(argc < 3) {
         printf("Utilizar:\n");
@@ -52,12 +56,13 @@ int main(int argc, char* argv[]) {
     client.sin_family = AF_INET;
     client.sin_addr.s_addr = htonl(INADDR_ANY);
     client.sin_port = htons(CLIENT_PORT);
-    
-    if ((bind(socket_fd, (struct sockaddr *)&client, sizeof(client))) != 0) {
+    int current_port = CLIENT_PORT;
+    while ((bind(socket_fd, (struct sockaddr *)&client, sizeof(client))) != 0) {
         printf("erro no bind\n");
-        close(socket_fd);
-        return(0);
+        current_port++;
+        client.sin_port = htons(current_port);
     }
+    printf("Fez o bind na porta: %d", current_port);
     
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr(server_ip);
@@ -69,13 +74,29 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     
+    
     strcpy(buffer, "Hello World!\0");
+    time(&current_time);
+    time(&start_time);
     while(1) {
         if ( ( send(socket_fd, buffer, sizeof(buffer), 0) ) < 0 ) {
             printf("erro na transmissao\n");
             close(socket_fd);
             return 0;
         }
+
+        count++;
+        time(&current_time);
+
+         if( difftime(current_time, start_time) >= 1 ) {
+            time(&current_time);
+            time(&start_time);
+            int bits_send_in_second = count * BUFFER_SIZE_BITS;
+            float rate_in_Kbits = (float)( bits_send_in_second / 1000);
+            // float rate_in_Mbits = (float)( rate_in_Kbits / 1000);
+            count = 0;
+            printf("Rate: %f Kbit/s\n", rate_in_Kbits);
+         }
     }
 
     printf("Fim da conexao\n");
